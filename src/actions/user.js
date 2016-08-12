d33deb0bef34de7f8c045296405badebfa1747b4
Nicolás Baglivo <nicolas.baglivo@gmail.com>
2016-08-12 18:15:50 +0200

    Update user.js

--- src/actions/user.js
+++ src/actions/user.js
@@ -5,3 +5,21 @@ export const FETCH_USER_REJECTED = 'FETCH_USER_REJECTED';
 export const fetchUser = username => ({ type: FETCH_USER, payload: username });
 export const fetchUserFulfilled = payload => ({ type: FETCH_USER_FULFILLED, payload });
 export const fetchUserError = error => ({ type: FETCH_USER_REJECTED, payload: error.xhr.response });
+
+import { FETCH_USER, fetchUserFulfilled, fetchUserError } from '../actions/user';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounce';
+import 'rxjs/add/operator/catch';
+import { ajax } from 'rxjs/observable/dom/ajax';
+import { Observable } from 'rxjs';
+
+export const fetchUser = action$ => (
+	action$.ofType(FETCH_USER)
+		.debounceTime(500)
+		.mergeMap(action =>
+			ajax.getJSON(`https://api.github.com/users/${action.payload}`)
+			.map(fetchUserFulfilled)
+			.catch(error => Observable.of(fetchUserError(error)))
+	    )
+);
